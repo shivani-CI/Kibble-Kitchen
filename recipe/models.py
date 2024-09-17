5bf09782c1d06c4a46017e2bbc031b1d3d12af5f
Shivani Seth <shivaniseth_1@hotmail.com>
2024-09-17 14:48:11 +0000

    Added a Recipe model

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
+
+STATUS =((0, "Draft"), (1, "Published"))
 # Create your models here.
+class Recipe(models.Model):
+    """
+    Stores a single recipe entry related to :model:`auth.User`. 
+    """
+    recipe_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(User, on_delete= models.CASCADE, related_name="recipe_book")
+    title = models.CharField(max_length=200, unique=True)
+    description =models.TextField()
+    prep_time = models.PositiveIntegerField(default=0)
+    cook_time = models.PositiveIntegerField(default=0)
+    difficulty = models.CharField(max_length=200, unique=True)
+    serves = models.IntegerField()
+    created_at = models.DateTimeField(auto_now=True)
+    status = models.IntegerField(choices=STATUS, default=0)
