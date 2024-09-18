4d3741903cc1597dd3cac547466d26e138baeb5f
Shivani Seth <shivaniseth_1@hotmail.com>
2024-09-18 10:10:19 +0000

    Added comment model for users to comment on recipes

--- recipe/models.py
+++ recipe/models.py
@@ -14,8 +14,23 @@ class Recipe(models.Model):
     description =models.TextField()
     prep_time = models.PositiveIntegerField(default=0)
     cook_time = models.PositiveIntegerField(default=0)
-    difficulty = models.CharField(max_length=200, unique=True)
+    difficulty = models.CharField(max_length=200)
     serves = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
     updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    """
+    Stores a single comment entry related to :model:`auth.User` and :model:`blog.Post`.
+    """
+    recipe_id = models.ForeignKey(Recipe, on_delete= models.CASCADE, related_name="comments")
+    user_id = models.ForeignKey(Recipe, on_delete= models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    approved = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+
+    
