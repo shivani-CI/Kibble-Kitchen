038293a0599c920f13bfa4e363893eeace46338d
Shivani Seth <shivaniseth_1@hotmail.com>
2024-09-25 13:10:24 +0000

    Added recipe form and updated views.py

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,8 +1,27 @@
-from .models import Comment
+from .models import Recipe, Comment
 from django import forms
+from django_summernote.widgets import SummernoteWidget
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
 
 
+class RecipeForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(RecipeForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.add_input(Submit('submit_recipe', 'Submit Recipe'))
+
+    class Meta:
+        model = Recipe
+        fields = ['title', 'description', 'prep_time', 'cook_time', 'difficulty', 'serves', 'status', 'image']
+        widgets = {
+            'description':SummernoteWidget()
+        }
+
+    
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,12 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views import generic
+from django.conf import settings
+from django.contrib import messages
 from django.http import JsonResponse
-from .models import Recipe, Ingredient, MealPlan, Comment
-from .forms import CommentForm
+from .models import Recipe, Ingredient, RecipeIngredient, MealPlan, Comment
+from .forms import CommentForm, RecipeForm
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -38,17 +41,34 @@ def get_recipe_detail(request, recipe_id):
             comment.user = request.user
             comment.recipe_id = recipe_id
             comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment submitted and awaiting approval'
+            )
     
     comment_form = CommentForm()
-    return render(request, 'recipe/recipe_detail.html', {'recipe': recipe,
-                                                         'comments': comments,
-                                                         'comment_count': comment_count,
-                                                         'comment_form': comment_form, })
+    return render(
+        request, 'recipe/recipe_detail.html', 
+        {   
+            'recipe': recipe,
+            'comments': comments,
+            'comment_count': comment_count,
+            'comment_form': comment_form,},
+    )
     
 def add_recipe(request):
     #TODO - how can you add variables to this function. How are they passed across?
     # recipe = get_object_or_404(queryset, recipe_id=recipe_id)
-    return render(request, 'recipe/add_recipe.html')
+    if request.method == 'POST':
+        form = RecipeForm(request.POST, request.FILES)
+        if form.is_valid():
+            recipe = form.save(commit=False)                       
+            recipe.user = request.user
+            recipe.save()            
+            return redirect('browse_recipes')
+    else:
+        form = RecipeForm()
+    return render(request, 'recipe/add_recipe.html', {'form': form})
 
 def create_meal_plan(request, meal_plan):
     #TODO - 
