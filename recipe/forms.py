f1712c1b62aa2ba8d1f8ed727e76e9adf8b339ca
Shivani Seth <shivaniseth_1@hotmail.com>
2024-10-02 10:23:51 +0000

    Simplified add recipe form for user experience

--- recipe/forms.py
+++ recipe/forms.py
@@ -1,4 +1,4 @@
-from recipe.models import Recipe, RecipeIngredient, MealPlan, Comment
+from recipe.models import Ingredient, Recipe, RecipeIngredient, MealPlan, Comment
 from django_summernote.widgets import SummernoteWidget
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
@@ -33,23 +33,24 @@ class RecipeForm(forms.ModelForm):
 
 class RecipeIngredientForm(forms.ModelForm):
     """
-    Add the ingredients for a recipe - also handles saving new ingredients
+    Add the ingredients for a recipe
     """
-    new_ingredient = forms.CharField(max_length=200, required=False, label="Add new ingredient")
+    ingredient = forms.CharField(max_length=200, required=True,
+                                 widget=forms.TextInput(attrs={'class': 'form-control',
+                                                               'placeholder': 'Enter ingredient name'}))
 
     class Meta:
         model = RecipeIngredient
         fields = ['ingredient', 'quantity', 'unit']
 
-    def clean(self):
-        cleaned_data = super().clean()
-        ingredient = cleaned_data.get('ingredient')
-        new_ingredient = cleaned_data.get('new_ingredient')
+    def clean_ingredient(self):
+        ingredient_name = self.cleaned_data.get('ingredient')
 
-        # Either select an ingredient or add a new ingredient
-        if not ingredient and not new_ingredient:
-            raise forms.ValidationError('Please either select an ingredient or add a new one.')
-        return cleaned_data
+        if ingredient_name:
+            ingredient, created = Ingredient.objects.get_or_create(name=ingredient_name)
+            return ingredient
+        else:
+            raise forms.ValidationError('This field is required')
 
 
 class MealPlanForm(forms.ModelForm):
@@ -59,7 +60,7 @@ class MealPlanForm(forms.ModelForm):
     start_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     end_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
     recipes = forms.ModelMultipleChoiceField(queryset=Recipe.objects.all(),
-    widget=forms.SelectMultiple())
+                                             widget=forms.SelectMultiple())
 
     def __init__(self, *args, **kwargs):
         super(MealPlanForm, self).__init__(*args, **kwargs)
