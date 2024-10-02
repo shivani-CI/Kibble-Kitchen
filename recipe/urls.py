86cb90a56f75d183fadcb9764f5bbd7fef58e8de
Shivani Seth <shivaniseth_1@hotmail.com>
2024-10-02 10:24:45 +0000

    Fixed add to meal plan button in recipe detail page

--- recipe/urls.py
+++ recipe/urls.py
@@ -1,20 +1,19 @@
 from recipe import views
-from django.contrib.auth import views as auth_views
 from django.urls import path
 
 
 urlpatterns = [
     path('', views.home_view, name='home'),
+    path('browse/', views.RecipeList.as_view(), name='browse_recipes'),
     path('add_recipe/', views.create_or_update_recipe, name='add_recipe'),
     path('edit_recipe/<int:recipe_pk>/', views.create_or_update_recipe, name='edit_recipe'),
     path('delete_recipe/<int:recipe_pk>/', views.delete_recipe, name='delete_recipe'),
-    path('browse/', views.RecipeList.as_view(), name='browse_recipes'),
     path('<int:recipe_pk>/', views.get_recipe_detail, name='get_recipe_detail'),
     path('<int:recipe_pk>/edit_comment/<int:comment_pk>', views.edit_comment, name='edit_comment'),
     path('<int:recipe_pk>/delete_comment/<int:comment_pk>', views.delete_comment, name='delete_comment'),
     path('add_ingredient/', views.add_ingredient, name='add_ingredient'),
     path('create_meal_plan/', views.create_meal_plan, name='create_meal_plan'),
     path('browse_meal_plans/', views.MealPlanList.as_view(), name='browse_meal_plans'),
-    path('meal_plan/<int:meal_plan_pk>/', views.get_meal_plan, name='get_meal_plan')
+    path('meal_plan/<int:meal_plan_pk>/', views.get_meal_plan, name='get_meal_plan'),
+    path('meal_plan-add_recipe', views.add_recipe_to_meal_plan, name='add_recipe_to_meal_plan')
 ]
-
---
+++ static/js/recipe.js
@@ -0,0 +1,52 @@
+const deleteRecipeButton = document.querySelector('#delete-recipe-btn');
+const recipeId = deleteRecipeButton.getAttribute('data-recipe-id');
+const addToMealPlanButton = document.querySelector('#add-recipe-to-meal-plan-btn');
+const mealPlanModal = new bootstrap.Modal(document.getElementById("mealPlanModal"));
+const mealPlanDropdown = document.getElementById("meal_plan_dropdown");
+const confirmAddToMealPlanButton = document.getElementById("confirm_add_to_meal_plan_btn");
+
+
+function displayMessage(message, type) {
+    console.log('We should see a mesage now');
+    const messageContainer = document.getElementById('message-container');
+    messageContainer.innerHTML = `<div class="alert alert-${type}">${message}</div>`;
+    setTimeout(() => {messageContainer.innerHTML = '';}, 3000);
+}
+
+deleteRecipeButton.addEventListener("click", function() {
+    deleteConfirm.href = `/delete_recipe/${recipeId}`;
+    deleteModal.show();
+});
+
+addToMealPlanButton.addEventListener("click", function() {
+    mealPlanModal.show();
+});
+
+confirmAddToMealPlanButton.addEventListener("click", function() {
+    const mealPlanId = mealPlanDropdown.value;
+
+    fetch(`/meal_plan-add_recipe`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': document.querySelector('[name=csrfmiddlewaretoken]').value
+        },
+        body: JSON.stringify({
+            'meal_plan_id': mealPlanId,
+            'recipe_id': recipeId
+        })
+    }).then(response => {
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Failed to add recipe to meal plan.');
+        }
+    }).then(data => {
+        mealPlanModal.hide();
+        if (data.success) {
+            displayMessage(data.message, 'success');
+        } else {
+            displayMessage(data.message, 'error');
+        }
+    });
+});
